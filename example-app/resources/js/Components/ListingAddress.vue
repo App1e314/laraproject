bb926920dae37381f3b321a1f3c3408bc456937f
unknown <liewweifeng@gmail.com>
2024-10-13 22:57:33 +0800

    first commit

---
+++ example-app/resources/js/Components/ListingAddress.vue
@@ -0,0 +1,16 @@
+<template> 
+<span>  {{ listing.street }} {{ listing.street_nr }}, {{ listing.city }}
+     </span>
+
+
+    
+</template>
+
+<script setup>
+
+ // import ListingAddress from '@/Components/ListingAddress.vue'
+
+  defineProps({
+    listing: Object,
+  })
+  </script>
---
+++ example-app/resources/js/Components/ListingSpace.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <span class="font-bold">{{ listing.beds }}</span> bds <span class="text-gray-400">| </span>  
+      <span class="font-bold">{{ listing.baths }}</span> ba <span
+        class="text-gray-400"
+      >| </span>
+      <span class="font-bold">{{ listing.area }}</span> m²
+    </div>
+  </template>
+
+
+
+
+
+<script setup>
+ // import ListingAddress from '@/Components/ListingAddress.vue'
+  defineProps({
+    listing: Object,
+  })
+  </script>
